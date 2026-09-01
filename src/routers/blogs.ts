757632e177e13e457d0daae7f679648a62fb6512
maakakasha <maakakasha1@gmail.com>
2026-09-01 19:56:03 +0400

    Update blogs.ts

--- src/routers/blogs.ts
+++ src/routers/blogs.ts
@@ -8,6 +8,7 @@ import { BlogController } from "../controllers/blogs.ts";
 import type { BlogOperationContract } from "../database/persistaece/blogOperationsContract.ts";
 import { validateGetByIdMiddleWare } from "../middleware/validateGetById.ts";
 import { GetBlogRequestError } from "../errors/invalidCreateBlogRequestError.ts";
+import { validateCreateBlogMiddleWare } from "../middleware/validateCreateBlog.ts";
 
 export function constructBlogRouter(
   persistanceOps: BlogOperationContract,
@@ -18,14 +19,6 @@ export function constructBlogRouter(
     "/blogs",
     async (req: Request, res: Response, next: NextFunction) => {
       const blogs = await persistanceOps.getAllBlogs();
-
-      if (!blogs) {
-        throw new GetBlogRequestError({
-          message: "No blogs found",
-          statusCode: 404,
-          code: "ERR_NF",
-        });
-      }
 
       res.send(blogs);
     },
@@ -44,6 +37,63 @@ export function constructBlogRouter(
           code: "ERR_NF",
         });
       }
+
+      res.send(blog);
+    },
+  );
+
+  blogRouter.delete(
+    "/blogs/:id",
+    validateGetByIdMiddleWare,
+    async (req: Request<{ id: string }>, res: Response) => {
+      try {
+        await persistanceOps.deleteBlog(Number(req.params.id));
+      } catch (e) {
+        throw new GetBlogRequestError({
+          message: "Blog not found",
+          statusCode: 404,
+          code: "ERR_NF",
+        });
+      }
+
+      res.send();
+    },
+  );
+
+  blogRouter.patch(
+    "/blogs/:id",
+    validateGetByIdMiddleWare,
+    async (req: Request<{ id: string }>, res: Response) => {
+      try {
+        await persistanceOps.updateBlog(Number(req.params.id), req.body);
+      } catch (e) {
+        throw new GetBlogRequestError({
+          message: "Blog not found",
+          statusCode: 404,
+          code: "ERR_NF",
+        });
+      }
+
+      res.send();
+    },
+  );
+
+  blogRouter.get(
+    "/blogs/search/:term",
+    async (req: Request<{ term: string }>, res: Response) => {
+      const blogs = await persistanceOps.fitlerBlogsBySearchTerm(
+        req.params.term,
+      );
+
+      res.send(blogs);
+    },
+  );
+
+  blogRouter.post(
+    "/blogs/create",
+    validateCreateBlogMiddleWare,
+    async (req: Request, res: Response) => {
+      const blog = await persistanceOps.createBlog(req.body);
 
       res.send(blog);
     },
